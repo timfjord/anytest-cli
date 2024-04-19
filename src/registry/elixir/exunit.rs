97d0dc1acac363a3048f71aa4d4e14573512ffcd
Tim Masliuchenko <timfjord@hey.com>
2024-04-19 16:05:01 +0100

    :broom:

--- src/registry/elixir/exunit.rs
+++ src/registry/elixir/exunit.rs
@@ -41,9 +41,8 @@ impl TestFramework for ExUnit {
 
     fn build_line_position_args(&self, context: &Context) -> Result<ArgsList, Box<dyn Error>> {
         let file_args = self.build_file_position_args(context)?;
-        let line_nr = context.line_nr_or_default();
 
-        if !is_mix(context) || line_nr < 2 {
+        if !is_mix(context) || context.line_nr_or_default() < 2 {
             return Ok(file_args);
         }
 
